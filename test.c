4cb3df00b91db5042d0d64ba9577d6ee6f439ec5
ZZZ <2893987468@qq.com>
2022-06-14 03:35:43 +0800

    test

---
+++ test.c
@@ -0,0 +1,63 @@
+//#define_CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+
+//int main()
+//{
+	//char ch = 'A';
+	//printf("%c\n",ch);
+	//int age = 20;
+	//printf("%d\n", age);
+	//printf("hehe\n");
+	//float f = 5.0;
+	//printf("%f\n",f);
+	//double d = 3.14;
+	//printf("%lf\n",d);
+	//return 0;
+//}
+//int main()
+//{
+	//printf("%d\n", sizeof(char));
+	//printf("%d\n", sizeof(short));
+	//printf("%d\n", sizeof(int));
+	//printf("%d\n", sizeof(long));
+	//printf("%d\n", sizeof(long long));
+	//printf("%d\n", sizeof(float));
+	//printf("%d\n", sizeof(double));
+	//return 0;
+//
+
+//int a = 100;
+//int main()
+//{
+//
+//	printf("%d\n", a);
+//	return 0;
+//
+//
+//
+//}
+//int main()
+//{
+//	int num1=10;
+//	int num2=10;
+//	int sum;
+//	
+//	scanf_s("%d%d", &num1, &num2);
+//	sum = num1 + num2;
+//	printf("%d\n", sum);
+//	return 0;
+//}
+int main()
+{
+	extern int gval;
+	printf("gval=%d\n", gval);
+
+
+
+
+
+	return 0;
+}
+
+
